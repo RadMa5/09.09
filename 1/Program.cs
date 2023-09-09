5c39d463f0baa1939d0f2fd12454737cc02523e4
RadMa <maks25022@gmail.com>
2023-09-09 17:24:44 +0500

    Start

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+void Counter(int max)
+{
+    if (max > 0)
+    {
+        Console.Write($"{max}, ");
+        max--;
+        Counter(max);
+    }
+}
+Console.Write("Input a number: ");
+Counter(Console.ReadLine());
