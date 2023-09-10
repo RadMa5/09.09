f8879469e48b4ed6337d7c2736b4b4cf66187a51
RadMa <maks25022@gmail.com>
2023-09-10 14:39:34 +0500

    Ackermann Function

---
+++ 3/Program3.cs
@@ -0,0 +1,11 @@
+﻿int Ack(int m, int n)
+{
+    if (m == 0) { return n + 1; }
+    else if (n == 0)
+    {
+        return Ack(m - 1, 1);
+    }
+    else { return Ack(m - 1, Ack(m, n - 1)); }
+}
+Console.WriteLine(Ack(2, 3));
+Console.WriteLine(Ack(3, 2));
